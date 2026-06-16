9974555a37348be008971b512287ea77f137b481
Soorya A P <165376922+Soorya005@users.noreply.github.com>
2026-06-16 11:11:56 +0530

    Update engine.py

--- detection/sigma/engine.py
+++ detection/sigma/engine.py
@@ -1,6 +1,1 @@
-from detection.sigma.rule_loader import load_rules
 
-
-def match_event(event: dict) -> list[dict]:
-    rules = load_rules()
-    return [rule for rule in rules if rule.get("enabled", True)]
